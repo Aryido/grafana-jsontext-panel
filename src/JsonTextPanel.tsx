a1d187f158988fb0c6c4768f5644a7edaf2b9582
Aryido <f104882013@gmail.com>
2022-01-12 15:45:46 +0800

    panel will show complete error message form backend

--- src/JsonTextPanel.tsx
+++ src/JsonTextPanel.tsx
@@ -4,7 +4,7 @@ import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
 
 export const JsonTextPanel: React.FC<PanelProps> = ({ data, width, height }) => {  
-  let value: string|undefined = findData(data);
+  let value: string | undefined = findData(data);
   return (
     <div style={{ overflow: 'auto', width, height }}>
       <div><JSONPretty data={value}></JSONPretty></div>
@@ -15,13 +15,12 @@ export const JsonTextPanel: React.FC<PanelProps> = ({ data, width, height }) => 
 function findData(data: PanelData): string | undefined {
   try {
     if(data.state == LoadingState.Error){
-      throw new Error(data.error?.message)
+      throw new Error();
     }
-    
     return data.series[0].fields[0].values.get(0);
   } catch (error) {
-    console.log(error);
-    return data.error?.message
+    console.log(data);
+    return JSON.stringify(data.error?.data);
   }
 }
 
